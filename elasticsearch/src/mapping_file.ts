2a0a549602f4fe7273da0594b7b15761dd1d10dc
D-sandaruwan <dsandaruwan7@gmail.com>
2023-01-17 17:12:29 +0530

    created the ts environment and added basic version of elasticsearch code

---
+++ elasticsearch/src/mapping_file.ts
@@ -0,0 +1,251 @@
+"use strict";
+import { Client } from "@elastic/elasticsearch"
+import "array.prototype.flatmap"
+
+import corpus from "../corpus.json";
+import keys from "../keys.json";
+
+const getStopWords = (keyWords: { [key: string]: string[] }) => {
+  const stopWords: string[] = [];
+  for (const property in keyWords) {
+    for (const key of keyWords[property]) {
+      stopWords.push(key);
+    }
+  }
+  return stopWords;
+};
+
+const client = new Client({
+  node: "https://localhost:9200",
+  auth: {
+    username: "elastic",
+    password: "-BYE5=PQ*+bWRkEKIyeS",
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+async function run() {
+  await client.indices.create({
+    index: "sinhala_songs_index",
+    body: {
+      settings: {
+        analysis: {
+          /**
+           * here the custom icu_tokenizer will be used 
+           * to get a better performance with
+           * asian languages
+           */
+          analyzer: {
+            // @ts-ignore
+            my_analyzer_icu: {
+              type: "custom",
+              tokenizer: "icu_tokenizer",
+              filter: ["my_ngram_filter", "my_stop_word_filter"],
+            },
+          },
+          /**
+           * custom token filters will be used
+           * because to get better performance with
+           * Sinhala language
+           */
+          filter: {
+            /**
+             * edge_ngram filter will be used
+             * to tokenize the words with minimum 3 letters to
+             * 18 letters
+             */
+            // @ts-ignore
+            my_ngram_filter: {
+              type: "edge_ngram",
+              min_gram: 3,
+              max_gram: 18,
+              side: "front",
+            },
+            /**
+             * custom stop word filter will be used
+             * to ignore some words introduced in the
+             * keys.json
+             */
+            // @ts-ignore
+            my_stop_word_filter: {
+              type: "stop",
+              ignore_case: true,
+              stopwords: getStopWords(keys)
+            },
+          },
+        },
+      },
+      mappings: {
+        properties: {
+          Composer_Sinhala: {
+            type: "text",
+            fields: {
+              raw: {
+                type: "keyword"
+              }
+            },
+            analyzer: "my_analyzer_icu",
+          },
+          Singer_Sinhala: {
+            type: "text",
+            fields: {
+              raw: {
+                type: "keyword"
+              }
+            },
+            analyzer: "my_analyzer_icu",
+          },
+          Lyricist_Sinhala: {
+            type: "text",
+            fields: {
+              raw: {
+                type: "keyword"
+              }
+            },
+            analyzer: "my_analyzer_icu",
+          },
+          Song_Sinhala: {
+            type: "text",
+            fields: {
+              raw: {
+                type: "keyword"
+              }
+            },
+            analyzer: "my_analyzer_icu",
+          },
+          Metaphor_1_Sinhala: {
+            type: "text",
+            fields: {
+              raw: {
+                type: "keyword"
+              }
+            },
+            analyzer: "my_analyzer_icu",
+          },
+          Metaphor_2_Sinhala: {
+            type: "text",
+            fields: {
+              raw: {
+                type: "keyword"
+              }
+            },
+            analyzer: "my_analyzer_icu",
+          },
+          Metaphor_3_Sinhala: {
+            type: "text",
+            fields: {
+              raw: {
+                type: "keyword"
+              }
+            },
+            analyzer: "my_analyzer_icu",
+          },
+          Subject_1: {
+            type: "text",
+            fields: {
+              raw: {
+                type: "keyword"
+              }
+            },
+            analyzer: "my_analyzer_icu",
+          },
+          Subject_2: {
+            type: "text",
+            fields: {
+              raw: {
+                type: "keyword"
+              }
+            },
+            analyzer: "my_analyzer_icu",
+          },
+          Subject_3: {
+            type: "text",
+            fields: {
+              raw: {
+                type: "keyword"
+              }
+            },
+            analyzer: "my_analyzer_icu",
+          },
+          Target_1: {
+            type: "text",
+            fields: {
+              raw: {
+                type: "keyword"
+              }
+            },
+            analyzer: "my_analyzer_icu",
+          },
+          Target_2: {
+            type: "text",
+            fields: {
+              raw: {
+                type: "keyword"
+              }
+            },
+            analyzer: "my_analyzer_icu",
+          },
+          Target_3: {
+            type: "text",
+            fields: {
+              raw: {
+                type: "keyword"
+              }
+            },
+            analyzer: "my_analyzer_icu",
+          },
+          Lyrics: {
+            "type": "text"
+          },
+          Interpretation_1: {
+            "type": "text"
+          },
+          Interpretation_2: {
+            "type": "text"
+          },
+          Interpretation_3: {
+            "type": "text"
+          },
+          Spotify_Plays: {
+            "type": "text"
+          },
+        }
+      }
+    },
+  })
+
+  const operations = corpus.flatMap(document => [{index: {_index: "sinhala_songs_index"}}, document])
+  const bulkResponse = await client.bulk({refresh: true, operations})
+
+  if (bulkResponse.errors) {
+    const erroredDocuments:any[] = []
+    // The items array has the same order of the dataset we just indexed.
+    // The presence of the `error` key indicates that the operation
+    // that we did for the document has failed.
+    bulkResponse.items.forEach((action, i) => {
+      const operation = Object.keys(action)[0]
+      // @ts-ignore
+      if (action[operation].error) {
+        erroredDocuments.push({
+          // If the status is 429 it means that you can retry the document,
+          // otherwise it's very likely a mapping error, and you should
+          // fix the document before to try it again.
+          // @ts-ignore
+          status: action[operation].status,
+          // @ts-ignore
+          error: action[operation].error,
+          operation: operations[i * 2],
+          document: operations[i * 2 + 1]
+        })
+      }
+    })
+    console.log(erroredDocuments)
+  }
+
+  const count = await client.count({ index: 'sinhala_songs_index' })
+  console.log(count)
+}
+
+run().catch(console.log);
